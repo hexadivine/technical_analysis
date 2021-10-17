48d77f6a933269d7166eb86bf232538a4f821c58
Hrushikesh <hd@Hrushikeshs-MacBook-Air.local>
2021-10-17 06:27:24 -0700

    .

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+long_description = 'A light-weight library which does not require any libraries to install and processes data from pandas, numpy or a normal python array with the help of threads.'
+
+setup(
+    name='technical_analysis',
+    description=long_description,
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas'
+        ],
+
+)
