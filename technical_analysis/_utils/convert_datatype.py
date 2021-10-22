b90421d815535ccd7b78e71c4657cf44ee044a6d
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2021-10-23 04:08:08 +0530

    Helps in converting datatype from and to numpy

---
+++ technical_analysis/_utils/convert_datatype.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+def convert_to_numpy(data):
+    if (str(type(data)) != "<class 'numpy.ndarray'>"):
+        return np.array(data), str(type(data))
+
+    return data, str(type(data))
+
+def convert_numpy(data, to):
+    if (to == "<class 'pandas.core.series.Series'>"):
+        return pd.Series(data)
+    elif (to == "<class 'list'>"):
+        return list(data)
+
+    return data
