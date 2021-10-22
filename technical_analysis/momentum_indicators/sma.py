1602b091a73691623db7539b670720f3a60253d3
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2021-10-23 04:07:22 +0530

    Optimized SMA

--- technical_analysis/momentum_indicators/sma.py
+++ technical_analysis/momentum_indicators/sma.py
@@ -1,43 +1,21 @@
+from technical_analysis._utils.convert_datatype import convert_numpy, convert_to_numpy
 import numpy as np
-import pandas as pd
+from math import nan
 
-class SMA():
-    def __init__(self, data, limit):
-        self.__sma = []
-        self.__data_type = None
+def sma(data, limit):
+    if (limit == 1):
+        return data
+    if (limit > len(data)):
+        return convert_numpy(np.full(len(data), nan), str(type(data))) 
 
-        if (str(type(data)) == "<class 'numpy.ndarray'>"):
-            self.__data_type = 'numpy'
-        elif (str(type(data)) == "<class 'pandas.core.series.Series'>"):
-            self.__data_type = 'pandas'
-        elif (str(type(data)) == "<class 'list'>"):
-            self.__data_type = 'list'
-        else:
-            raise ValueError
+    data, type_of_data = convert_to_numpy(data)
+
+    continues_sum = np.cumsum(data, dtype='float')
+    continues_sum[limit:] = continues_sum[limit:] - continues_sum[:-limit]
+    moving_avarage = continues_sum[limit-1:]/limit
     
-        self.__sma = self.__calculate_sma(np.array(data), limit)
+    moving_avarage = np.concatenate([(limit-1)*[nan], moving_avarage])
 
-    def __calculate_sma(self, data, limit):
-        if (limit > len(data)):
-            return np.full(len(data), float("nan"))
+    return convert_numpy(moving_avarage, to=type_of_data)
 
-        continues_sum = np.cumsum(data, dtype='float')
-        continues_sum[limit:] = continues_sum[limit:] - continues_sum[:-limit]
-        moving_avarage = continues_sum[limit-1:]/limit
-        
-        moving_avarage = np.concatenate([(limit-1)*[float("nan")], moving_avarage])
-        return np.round(moving_avarage,2)
-
-    def get_sma(self):
-        if (self.__data_type == 'numpy'):
-            return self.__sma
-        elif (self.__data_type == 'pandas'):
-            return pd.Series(self.__sma)
-        elif (self.__data_type == 'list'):
-            return list(self.__sma)
-        else:
-            raise ValueError
-
-def sma(data, limit=14):
-    technical_analysis = SMA(data, limit)
-    return technical_analysis.get_sma()
+    
