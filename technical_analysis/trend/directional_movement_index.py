64b11eba856e04d9ccd7fc99a47a0a9766d689c1
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2021-10-29 03:18:02 +0530

    fixed bug - rounding befor converting to original datatype

--- technical_analysis/trend/directional_movement_index.py
+++ technical_analysis/trend/directional_movement_index.py
@@ -12,11 +12,11 @@ def DMI(high, low, close, limit, moving_average_func=ta.EMA, alpha=1.071421):
         raise ValueError("Some High values are smaller than Low values")
 
     # find di
-    plus_di = ta.PLUS_DI(high, low, close, limit, moving_average_func, alpha)
-    minus_di = ta.MINUS_DI(high, low, close, limit, moving_average_func, alpha)
+    plus_di = np.round(ta.PLUS_DI(high, low, close, limit, moving_average_func, alpha), 2)
+    minus_di = np.round(ta.MINUS_DI(high, low, close, limit, moving_average_func, alpha), 2)
 
     # find adx
-    adx = ta.ADX(high, low, close, limit, moving_average_func, alpha)
+    adx = np.round(ta.ADX(high, low, close, limit, moving_average_func, alpha), 2)
 
-    return np.round(convert_numpy(plus_di, to=original_datatype),2), np.round(convert_numpy(minus_di, to=original_datatype),2), np.round(convert_numpy(adx, to=original_datatype),2)
+    return convert_numpy(plus_di, to=original_datatype), convert_numpy(minus_di, to=original_datatype), convert_numpy(adx, to=original_datatype)
 
