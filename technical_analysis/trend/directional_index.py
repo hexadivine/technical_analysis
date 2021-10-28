130b28c2ad06e7ba85672e7f66edd27bc8c39dd8
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2021-10-29 02:05:46 +0530

    added PLUS_DI and MINUS_DI with the help of FIND_DI

---
+++ technical_analysis/trend/directional_index.py
@@ -0,0 +1,23 @@
+import technical_analysis as ta
+from technical_analysis._utils.convert_datatype import convert_numpy, convert_to_numpy
+
+def FIND_DI(high, low, close, limit, direction_type='PLUS', moving_average_func=ta.EMA, alpha=1.071421):
+    # validate data
+    high, original_datatype = convert_to_numpy(high)
+    low, _ = convert_to_numpy(low)
+    close, _ = convert_to_numpy(close)
+
+    find_dm = ta.FIND_DM(high, low, direction_type)
+    smoothed_find_dm = moving_average_func(find_dm, limit, alpha)
+
+    average_true_range = ta.ATR(high, low, close, limit, moving_average_func, alpha)
+
+    find_di = 100 * smoothed_find_dm/average_true_range
+    
+    return convert_numpy(find_di, original_datatype)
+
+def PLUS_DI(high, low, close, limit, moving_average_func=ta.EMA, alpha=1.071421):
+    return FIND_DI(high, low, close, limit, 'PLUS', moving_average_func, alpha)
+
+def MINUS_DI(high, low, close, limit, moving_average_func=ta.EMA, alpha=1.071421):
+    return FIND_DI(high, low, close, limit, 'MINUS', moving_average_func, alpha)
