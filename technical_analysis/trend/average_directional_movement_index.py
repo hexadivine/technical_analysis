256c0ce5b273c79da25cf3c2fe9684bfe1e872df
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2021-10-29 02:07:51 +0530

    improved ADX function defination

--- technical_analysis/trend/average_directional_movement_index.py
+++ technical_analysis/trend/average_directional_movement_index.py
@@ -1,6 +1,20 @@
+from technical_analysis._utils.convert_datatype import convert_to_numpy, convert_numpy
 import technical_analysis as ta
 
-def ADX(high, low, close, limit, opt_ATR_params={'ma_function':ta.EMA, 'alpha':1.071421}):
-    # get adx from dmi
-    _, _, adx = ta.DMI(high, low, close, limit, opt_ATR_params)
-    return adx
+def ADX(high, low, close, limit, moving_average_func=ta.EMA, alpha=1.071421):
+    # validate data
+    high, original_datatype = convert_to_numpy(high)
+    low, _ = convert_to_numpy(low)
+    close, _ = convert_to_numpy(close)
+
+    # find di
+    plus_di = ta.PLUS_DI(high, low, close, limit, moving_average_func, alpha)
+    minus_di = ta.MINUS_DI(high, low, close, limit, moving_average_func, alpha)
+
+    # calculate dx
+    dx = abs(plus_di - minus_di)/abs(plus_di + minus_di)
+
+    # get average directional movement index using ema on dx
+    adx = 100*moving_average_func(dx, limit, alpha)
+
+    return convert_numpy(adx, original_datatype)
