60b8429d1783107c43f10c07bbdc6ec2bb642120
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2021-10-29 02:50:14 +0530

    implimented ta.MA

--- technical_analysis/trend/directional_index.py
+++ technical_analysis/trend/directional_index.py
@@ -7,13 +7,14 @@ def FIND_DI(high, low, close, limit, direction_type='PLUS', moving_average_func=
     low, _ = convert_to_numpy(low)
     close, _ = convert_to_numpy(close)
 
+    # find directional movement
     find_dm = ta.FIND_DM(high, low, direction_type)
-    smoothed_find_dm = moving_average_func(find_dm, limit, alpha)
-
+    
+    smoothed_find_dm = ta.MA(find_dm, limit, moving_average_func, {'alpha': alpha})
     average_true_range = ta.ATR(high, low, close, limit, moving_average_func, alpha)
 
     find_di = 100 * smoothed_find_dm/average_true_range
-    
+
     return convert_numpy(find_di, original_datatype)
 
 def PLUS_DI(high, low, close, limit, moving_average_func=ta.EMA, alpha=1.071421):
