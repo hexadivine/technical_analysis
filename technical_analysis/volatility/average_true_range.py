3d910db10a612363d84410ca7c98dec83bcfbb2e
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2021-10-29 03:18:31 +0530

    fixed bug - rounding befor converting to original datatype

--- technical_analysis/volatility/average_true_range.py
+++ technical_analysis/volatility/average_true_range.py
@@ -4,6 +4,6 @@ import numpy as np
 def ATR(high, low, close, limit, moving_average_func=ta.EMA, alpha=1.071421):
     # get true range
     tr = ta.TRANGE(high, low, close)
-    atr = ta.MA(tr, limit, moving_average_func, {'alpha':alpha})
+    atr = np.round(ta.MA(tr, limit, moving_average_func, {'alpha':alpha}), 2)
 
-    return np.round(atr, 2)
+    return atr
