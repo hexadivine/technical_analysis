Fixed array limit overflow bug

technical_analysis/moving_average/_smoothed_moving_average.py
@@ -8,19 +8,23 @@ def SMMA(data, limit):
     if (limit > data_len):
         return convert_numpy(np.full(data_len, np.nan), str(type(data))) 
 
-    start=0
-    while (start < data_len and np.isnan(data[start])):
-        start+=1
+    count_nan=0
+    while (count_nan < data_len and np.isnan(data[count_nan])):
+        count_nan+=1
+
+    if (count_nan > 0):
+        data[count_nan:] = SMMA(data[count_nan:], limit)
+        return data
 
     sum=0
-    for index in range(start, start+limit-1):
+    for index in range(limit-1):
         sum += data[index]
         data[index] = np.nan
     
-    sma = (sum+data[start+limit-1])/limit
+    sma = (sum+data[limit-1])/limit
     smma = sma/limit
     
-    for index in range(start+limit-1, data_len-1):
+    for index in range(limit-1, data_len-1):
         old_data_val = data[index]
         data[index] = smma
         smma = (smma*limit - smma + old_data_val)/limit