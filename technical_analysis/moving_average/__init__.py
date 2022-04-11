eed216a9a398716dcfe7f307406adf4abdc74bb0
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2022-04-11 16:59:46 -0500

    Removed MA funtion

--- technical_analysis/moving_average/__init__.py
+++ technical_analysis/moving_average/__init__.py
@@ -2,5 +2,4 @@ from ._simple_moving_avarage import SMA
 from ._exponential_moving_average import EMA
 from ._running_moving_average  import RMA, SMMA
 
-from ._moving_average import MA
 from ._ma_cross import MA_CROSS
